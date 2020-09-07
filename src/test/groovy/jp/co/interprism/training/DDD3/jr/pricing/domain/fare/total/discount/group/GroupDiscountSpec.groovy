2882d8de6adc4902042bd3f33e49c1dc866f4b99
komatsu_yuki <ip.b106.komatsu.yuki@gmail.com>
2020-09-07 17:56:44 +0900

    GroupDiscountクラスのテストを書いた

--- src/test/groovy/jp/co/interprism/training/DDD3/jr/pricing/domain/fare/total/discount/group/GroupDiscountSpec.groovy
+++ src/test/groovy/jp/co/interprism/training/DDD3/jr/pricing/domain/fare/total/discount/group/GroupDiscountSpec.groovy
@@ -1,6 +1,8 @@
 package jp.co.interprism.training.DDD3.jr.pricing.domain.fare.total.discount.group
 
 import jp.co.interprism.training.DDD3.jr.pricing.domain.boarding.date.BoardingDate
+import jp.co.interprism.training.DDD3.jr.pricing.domain.fare.total.discount.group.member.AdultsCount
+import jp.co.interprism.training.DDD3.jr.pricing.domain.fare.total.discount.group.member.ChildrenCount
 import jp.co.interprism.training.DDD3.jr.pricing.domain.fare.total.discount.group.member.MembersCount
 import jp.co.interprism.training.DDD3.jr.pricing.domain.fare.unit.FareCount
 import spock.lang.Specification
@@ -10,22 +12,45 @@ import java.time.LocalDate
 
 @Unroll
 class GroupDiscountSpec extends Specification {
-    def "団体人数が#団体人数のときに利用可能かどうか"() {
+    def "団体人数#totalのときに利用可能かどうか"() {
         setup:
         def boardingDate = new BoardingDate(LocalDate.of(2020, 9, 3))
-        def group = Stub(Group)
-
-        group.sumMembersCount() >> new MembersCount(new FareCount(団体人数))
+        def adultsCount = new AdultsCount(new MembersCount(new FareCount(adults)))
+        def childrenCount = new ChildrenCount(new MembersCount(new FareCount(children)))
+        def group = new Group(adultsCount, childrenCount)
 
         when:
         def groupDiscount = new GroupDiscount(group, boardingDate)
 
         then:
+        assert total == adults + children
         assert groupDiscount.isAvailable() == result
 
         where:
-        団体人数 || result
-        7    || false
-        8    || true
+        adults | children || total || result
+        5      | 3        || 8     || true
+        4      | 3        || 7     || false
+    }
+
+    def "大人と子どもの比に偏りな場合"() {
+        setup:
+        def boardingDate = new BoardingDate(LocalDate.of(2020, 9, 3))
+        def adultsCount = new AdultsCount(new MembersCount(new FareCount(adults)))
+        def childrenCount = new ChildrenCount(new MembersCount(new FareCount(children)))
+        def group = new Group(adultsCount, childrenCount)
+
+        when:
+        def groupDiscount = new GroupDiscount(group, boardingDate)
+
+        then:
+        assert total == adults + children
+        assert groupDiscount.isAvailable() == result
+
+        where:
+        adults | children || total || result
+        8      | 0        || 8     || true
+        0      | 8        || 8     || true
+        7      | 0        || 7     || false
+        0      | 7        || 7     || false
     }
 }
