b5884e7a6ab1a7545ef984c0c0fe8bc3f983b619
komatsu_yuki <ip.b106.komatsu.yuki@gmail.com>
2020-09-11 11:15:41 +0900

    GroupDiscountクラスのテストを書いた

---
+++ src/test/groovy/jp/co/interprism/training/DDD3/jr/pricing/domain/fare/surcharge/superexpress/seat/free/SuperExpressSurchargeFreeSeatSpec.groovy
@@ -0,0 +1,4 @@
+package jp.co.interprism.training.DDD3.jr.pricing.domain.fare.surcharge.superexpress.seat.free
+
+class SuperExpressSurchargeFreeSeatSpec {
+}
--- src/test/groovy/jp/co/interprism/training/DDD3/jr/pricing/domain/fare/total/group/GroupDiscountSpec.groovy
+++ src/test/groovy/jp/co/interprism/training/DDD3/jr/pricing/domain/fare/total/group/GroupDiscountSpec.groovy
@@ -56,59 +56,6 @@ class GroupDiscountSpec extends Specification {
         0      | 7        || 7     || false
     }
 
-    def "正常系: 合計#total人の団体は#freeTotal人無料になる"() {
-        setup:
-        def boardingDate = new BoardingDate(LocalDate.of(2020, 9, 3))
-        def adultsCount = new AdultsCount(new MembersCount(new FareCount(adults)))
-        def childrenCount = new ChildrenCount(new MembersCount(new FareCount(children)))
-        def group = new Group(adultsCount, childrenCount)
-
-        when:
-        def discount = new GroupDiscount(group, boardingDate)
-        def groupForPayment = discount.getGroupForPayment()
-
-        then:
-        assert adults + children == total
-        assert adults - groupForPayment.adultsCount.membersCount.fareCount.value == freeAdults
-        assert children - groupForPayment.childrenCount.membersCount.fareCount.value == freeChildren
-        assert freeAdults + freeChildren == freeTotal
-
-        where:
-        adults | children | total || freeAdults | freeChildren | freeTotal
-        21     | 9        | 30    || 0          | 0            | 0
-        20     | 11       | 31    || 1          | 0            | 1
-        37     | 13       | 50    || 1          | 0            | 1
-        36     | 15       | 51    || 2          | 0            | 2
-        75     | 25       | 100   || 2          | 0            | 2
-        78     | 23       | 101   || 3          | 0            | 3
-        120    | 30       | 150   || 3          | 0            | 3
-        115    | 36       | 151   || 4          | 0            | 4
-    }
-
-    def "正常系: 無料人数が大人の人数より多い場合"() {
-        setup:
-        def boardingDate = new BoardingDate(LocalDate.of(2020, 9, 3))
-        def adultsCount = new AdultsCount(new MembersCount(new FareCount(adults)))
-        def childrenCount = new ChildrenCount(new MembersCount(new FareCount(children)))
-        def group = new Group(adultsCount, childrenCount)
-
-        when:
-        def discount = new GroupDiscount(group, boardingDate)
-        def groupForPayment = discount.getGroupForPayment()
-
-        then:
-        assert adults + children == total
-        assert adults - groupForPayment.adultsCount.membersCount.fareCount.value == freeAdults
-        assert children - groupForPayment.childrenCount.membersCount.fareCount.value == freeChildren
-        assert freeAdults + freeChildren == freeTotal
-
-        where:
-        adults | children | total || freeAdults | freeChildren | freeTotal
-        0      | 31       | 31    || 0          | 1            | 1
-        1      | 50       | 51    || 1          | 1            | 2
-        2      | 200      | 202   || 2          | 3            | 5
-    }
-
     def "正常系: #month月#date日の一人分運賃10000円の割引後運賃が#result円"() {
         setup:
         def boardingDate = new BoardingDate(LocalDate.of(2020, month, date))
